228849d77cacf8d97e04066a1c2456ee1e15a487
Ganesh-Alibilli <141898566+Ganesh-Alibilli@users.noreply.github.com>
2023-08-30 11:00:58 +0530

    Add files via upload

---
+++ vehiclesulike.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Vehiclesulike(p) {
+  return (
+    <div style={{border:"2px solid black",width:"160px",margin:"20px"}}>
+      <h3>{p.heading}</h3>
+      <p>{p.para}</p>
+      
+
+    </div>
+  )
+}
+
+export default Vehiclesulike
