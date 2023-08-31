8d45c1bd386d119f09e1810d79420ecd2df0dcf1
Ganesh-Alibilli <141898566+Ganesh-Alibilli@users.noreply.github.com>
2023-08-31 10:22:54 +0530

    Update vehiclesulike.js

--- vehiclesulike.js
+++ vehiclesulike.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Vehiclesulike(p) {
   return (
-    <div style={{border:"2px solid black",width:"160px",margin:"20px"}}>
+    <div style={{border:"2px solid black",width:"190px",margin:"20px"}}>
       <h3>{p.heading}</h3>
       <p>{p.para}</p>
       
